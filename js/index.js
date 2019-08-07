39ae39deb172877634ace3835e085999f5633593
Hector Valdez <hvaldez180@gmail.com>
2019-08-07 03:10:09 -0400

    added some eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const logo = document.querySelector(".logo-heading");
+
+logo.addEventListener("click", function(){
+    console.log("You clicked the logo")
+});
+
+const navBar = document.querySelectorAll("a");
+
+navBar.forEach(element => {
+    element.addEventListener("mouseover", function(){
+        element.style.color = "blue";
+    });
+    element.addEventListener("mouseleave", function() {
+        element.style.color = "black";
+    });
+});
+
+const funnyBus = document.querySelector(".intro img");
+
+funnyBus.addEventListener("dblclick", function(){
+    console.log("Stop clicking me!")
+})
+
+const textChange = document.querySelector(".intro h2");
+
+textChange.addEventListener("select", function(){
+    //textChange.style.textColor = "red"
+});
