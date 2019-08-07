0fe7127800860decada7604b8b224b464463ae5f
Hector Valdez <hvaldez180@gmail.com>
2019-08-07 03:42:22 -0400

    added more js

--- js/index.js
+++ js/index.js
@@ -22,8 +22,45 @@ funnyBus.addEventListener("dblclick", function(){
     console.log("Stop clicking me!")
 })
 
-const textChange = document.querySelector(".intro h2");
+//5
+const textChange = document.querySelector(".intro");
 
-textChange.addEventListener("select", function(){
-    //textChange.style.textColor = "red"
+textChange.addEventListener("load", function(){
+    textChange.style.textColor = "red";
 });
+
+
+//6
+const testingSelector = document.querySelector(".intro h2");
+
+testingSelector.addEventListener("select", function(){
+
+})
+
+//7
+const testingFocus = document.querySelector(".img-content img")
+
+testingFocus.addEventListener("focus", function() {
+    
+})
+
+//8
+const testingBlur = document.querySelector(".content-destination");
+
+testingBlur.addEventListener("blur", function() {
+
+})
+
+//9
+const testingMouseMove = document.querySelector(".content-destination h2");
+
+testingMouseMove.addEventListener("mousemove", function() {
+
+})
+
+//10
+const testingMouseUp = document.querySelector(".content-pick h4");
+
+testingMouseUp.addEventListener("mouseup", function() {
+
+})
